6f2c7c54631f08f5ef00868a2dde510b76ce532b
Koki TSURIMOTO <tsurimoto@ellipsys.co.jp>
2022-12-31 03:53:28 +0000

    Update backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
-from .utils.spa_staticfiles import SPAStaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+# from .utils.spa_staticfiles import SPAStaticFiles # For StaticFiles for SPA (React, Vue, etc.)
+from .schemas import ItemCreate
 
 
 app = FastAPI()
+
+db_items = {
+    1: {"item_id": 1, "text": "item1"},
+    2: {"item_id": 2, "text": "item2"}
+}
 
 
 @app.get("/api/v1/hello")
@@ -11,8 +18,21 @@ def read_root():
 
 
 @app.get("/api/v1/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    try:
+        return db_items[item_id]
+    except KeyError:
+        raise HTTPException(404, "Not Found")
 
 
-app.mount("", SPAStaticFiles(directory="static_files"), name="static_files")
+@app.post("/api/v1/items")
+def post_item(item: ItemCreate):
+    new_item = {"item_id": max(db_items) + 1, "text": item.text}
+    db_items[new_item["item_id"]] = new_item
+    return new_item
+
+
+app.mount("", StaticFiles(directory="static_files",
+          html=True), name="static_files")
+
+# app.mount("", SPAStaticFiles(directory="static_files"), name="static_files")
